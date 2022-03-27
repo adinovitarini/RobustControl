d7a63bcf72c4096775ee54b4ca5e41629c012e96
Adi Novitarini <adinovitarini@gmail.com>
2022-03-27 13:55:29 +0700

    Update HinfConDsgnSpinningSatellite.m

--- HinfConDsgnSpinningSatellite.m
+++ HinfConDsgnSpinningSatellite.m
@@ -8,6 +8,10 @@ Pnom = ss(Pnom,'min')
 SystemPole = pole(Pnom);
 SystemZero = tzero(Pnom);
 nmeas = 2; ncon = 2;
+Ms = 2; A = 1e-2; wb = 11.5; 
+Ms2 = 8;wb2 = 20;
+wP2 = tf([1/Ms2 wb2], [1 wb2*A]);
+WP2 = eye(2)*wP2; WP2 = ss(WP2) ;
 %% Plant with uncertainty
 for i = 1:2
     k(i) = ureal('k',1,'Range',[0.8,1.2]);
@@ -24,11 +28,25 @@ WM = ss(WM);
 wM2= tf([tau r0], [tau/rinf2 1]);
 WM2 = eye(2)*wM2;
 WM2 = ss(WM2) ;
-%% %% Trade off gamma based on Gmax and Gmin
+%% Multiplicative Output Uncertainty 
+time = [0:0.01:3];
+step_ref = ones(1,length(time));
+ref = [0.1*step_ref' 0*step_ref'];
+ref2 = [0.1*sin(10*time)' 0.1*cos(10*time)'];
+wP = tf([1/Ms wb], [1 wb*A]);
+WP = eye(2)*wP; WP = ss(WP) ;
+systemnames = 'Pnom WP2 WM';
+inputvar = '[w(2);u(2)]';
+outputvar = '[WP2;WM;-w-Pnom]';
+input_to_Pnom= '[u]';
+input_to_WP = '[w+Pnom]';
+input_to_WM = ' [Pnom]';
+G = sysic
+[Khiold,CLhi,ghiold,hiinfo] = hinfsyn(G,nmeas,ncon);
+Fhiold=loopsens(Pnom,Khiold);
+[SVold,wold] = sigma(CLhi);
+%% %% %% Trade off gamma based on Gmax and Gmin
 %Ms = 8 dan wb = 20
-Ms2 = 8;wb2 = 20;
-wP2 = tf([1/Ms2 wb2], [1 wb2*A]);
-WP2 = eye(2)*wP2; WP2 = ss(WP2) ;
 systemnames = 'Pnom WP WM';
 inputvar = '[w(2);u(2)]';
 outputvar = '[WP;WM;-w-Pnom]';
@@ -41,26 +59,6 @@ Gnew = sysic
 lft_norm = hinfnorm(lft(Gnew,Khi));
 [SV,w] = sigma(CLHi);
 [SVF,wF] = sigma(CLHiF);
-%% Multiplicative Output Uncertainty 
-time = [0:0.01:3];
-step_ref = ones(1,length(time));
-ref = [0.1*step_ref' 0*step_ref'];
-ref2 = [0.1*sin(10*time)' 0.1*cos(10*time)'];
-Ms = 2; A = 1e-2; wb = 11.5; 
-wP = tf([1/Ms wb], [1 wb*A]);
-WP = eye(2)*wP; WP = ss(WP) ;
-systemnames = 'Pnom WP2 WM';
-inputvar = '[w(2);u(2)]';
-outputvar = '[WP2;WM;-w-Pnom]';
-input_to_Pnom= '[u]';
-input_to_WP = '[w+Pnom]';
-input_to_WM = ' [Pnom]';
-G = sysic
-
-[Khiold,CLhi,ghiold,hiinfo] = hinfsyn(G,nmeas,ncon);
-Fhiold=loopsens(Pnom,Khiold);
-[SVold,wold] = sigma(CLhi);
-
 %% Plotting Figure
 figure(1);clf
 pzmap(tf(N,D));
@@ -98,24 +96,20 @@ hl = title('Time response with $\omega=[0.1step;0]^{T}$');
 set(hl ,'Interpreter','latex');
 legend('$P_{1}(s)$','$$\tilde{P}_{1}(s)$$','$\omega_{1}(s)$','$P_{2}(s)$','$$\tilde{P}_{2}(s)$$','$\omega_{2}(s)$','Interpreter', 'LaTeX');
 grid on
-figure(6);clf
-lsim(Pnom(1,:),ref2,time','r')
-hold on
-lsim(P(1,:),ref2,time','b')
-hold on
-plot(time',ref2(:,1),'-.r')
-lsim(Pnom(2,:),ref2,time','m')
-hold on
-lsim(P(2,:),ref2,time','c')
-hold on
-plot(time',ref2(:,2),'-.m')
+figure(6);clf;
+lsim(Pnom(1,:),ref2,time','r');hold on
+lsim(P(1,:),ref2,time','b');hold on
+plot(time',ref2(:,1),'-.r');hold on
+lsim(Pnom(2,:),ref2,time','m');hold on
+lsim(P(2,:),ref2,time','c');hold on
+plot(time',ref2(:,2),'-.m')'
 hl = title('Time response with $\omega=[0.1sin(\omega t);0.1cos(\omega t)]^{T}$');
 set(hl ,'Interpreter','latex');
 legend('$P_{1}(s)$','$$\tilde{P}_{1}(s)$$','$\omega_{1}(s)$','$P_{2}(s)$','$$\tilde{P}_{2}(s)$$','$\omega_{2}(s)$','Interpreter', 'LaTeX');
 grid on
 figure(7);clf
 bodemag(WP(1,1),'r');hold on;bodemag(WP2(1,1),'b')
-hold on
+hold on/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*
 bodemag(1/WM(1,1),'g')
 hl = legend('$\omega_b(s)=11.5$','$\omega_b(s)=20$','$\underline{\sigma}(WM^{-1})$')
 set(hl ,'Interpreter','latex')
